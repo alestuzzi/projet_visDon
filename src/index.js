8d676b107ef546360f10fa6a5a2a6351daca4b58
Adrien Lestuzzi <adrien.lestuzzi@heig-vd.ch>
2019-05-11 10:44:36 +0200

    push du samedi 11

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const fetch = require('node-fetch')
 const data = require('./solaire.json')
 const communes = require('./communes.json')
 import L from 'leaflet';
+import $ from 'jquery';
+const { LV03toWGS } = require('swiss-projection')
 
 // GMDNR
 // MunicipalityNumber
@@ -9,31 +11,62 @@ import L from 'leaflet';
 const getCommune = (MunicipalityNumber, features) =>
   features.find(f => f.properties.GMDNR === Number(MunicipalityNumber))
 
-const resultat = data.map(d => {
-  const feature = getCommune(d.MunicipalityNumber, communes.features)
-  if (feature) {
-    return {
-      ...feature,
-      properties: {
-        ...feature.properties,
-        ...d,
-      }
-    }
-  }
-  return d
-})
 
-const map = initMap('map').setView([46.5579, 9.5471], 9)
 
-const osmCH = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
+const map = L.map('map').setView([46.7965, 8.115], 9)
+
+const osmCH = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 8,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
+osmCH.addTo(map)
+
+// trier les cantons pour obtenir les 5 premières communes 
+
+//console.log(data[i].properties.Canton);
 /*
-console.log(
-  JSON.stringify(resultat.filter(f => f.type === 'Feature'))
-)
+const resultat = data.map(d => {
+  d.sort(data.properties.Canton)  //tri par canton
+  //tri par potentiel solaire
+  //ne séléctionner que les 5 premiers
+  //ajouter au tableau
+})
+
+var i;
+for (i = 0; i < 6; i++) {
+  billboardLeHobbit.map();
+} 
+
+
+*/
+/*
+const years = Object.keys(o)
+ .filter(key => key !== 'Year')
+ .map(Number)
+
+const list = years.map(year => ({
+ year,
+ population: Number(o[year])
+}))
+
+console.log(list)
 */
 
-osmCH.addTo(map)
+
+// afficher les communes sur la carte
+
+const getCoords = feature => LV03toWGS([
+  feature.properties.X_CNTR,
+  feature.properties.Y_CNTR,
+])
+
+const d = data.map(getCoords)
+
+$.each(data, function(i, obj) {
+  L.marker([d[i][1], d[i][0]]).addTo(map);
+});
+
+// rendre les points de communes cliquable
+
+// afficher les informations relatives à la commune
